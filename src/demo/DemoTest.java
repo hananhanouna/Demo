1774ddc5f85070b6b28fc3e4f21fcbb4d1ee475a
17039 <17039@DESKTOP-Q54OCII.fios-router.home>
2022-08-01 19:28:03 -0400

    Implimentation for US123

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -1,7 +1,13 @@
 package demo;
 
 public class DemoTest {
-public static void main(String[] args) {
-	System.out.println("Testing");
-}
+	public static void main(String[] args) {
+		System.out.println("Testing");
+	}
+
+// for US123.....
+
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
 }
